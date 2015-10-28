9c546a9cac4b34f20933dcb5d1197895d63fc995
sarahbroat <sarahbroat@192.168.1.8>
2015-10-28 19:05:24 -0400

    October 28 - Start of Team Assignment 6; Issues doing test run

---
+++ sphinxIsland/src/byui/cit260/sphinxIsland/view/StartProgramView.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.sphinxIsland.view;
+
+/**
+ *
+ * @author sarahbroat
+ */
+public class StartProgramView {
+    
+    public void startProgramView() {
+    }
+    public void startProgram() {
+            
+            // display the banner screen
+            this.displayBanner();
+            
+            // prompt the player to enter their name and retrieve player name
+            
+            // create and save the player object
+            
+            // display a personalized welcome message
+            
+            // display the main menu
+            
+        }
+
+    private void displayBanner() {
+        System.out.println("\n\n*****************************************");
+                
+        System.out.println("\n*                                      *");
+        
+        System.out.println("\n*Welcome to the Sphinx' Island! *"
+                          +"\n*In this game you will solve a  *"
+                          +"\n*series of riddles give to you by *"
+                          +"\n*a cunning Sphinx and her sisters.*");
+        
+        System.out.println("\n*                                         *"
+                           +"\n*You are traveling the world in pursuit *"
+                           +"\n*of knowledge and after a ferocious *"
+                           +"\n*storm have washed ashore on a strange *"
+                           +"\n*tropical island. You have no possessions, *"
+                           +"\n*but have been blessed with a sharp mind. *"
+                           +"\n*To leave the island and continue your *"
+                           +"\n*travels, you must successfully best three *"
+                           +"\n*sphinxes in a battle of wits and collect *"
+                           +"\n*the precious gems that they will give you *"
+                           +"\n*as proof of you intellectual mastery to *"
+                           +"\n*exchange for a raft that will take you *"
+                           +"\n*safely from their clutches. If you fail, *"
+                           +"\n*you will be turned to stone and kept as *"
+                           +"\n*a monument on their island for all eternity.*");
+        
+        System.out.println("\n*                                             *"
+                          + "\n*Good luck- hope you're feeling clever today!*"
+                          + "\n*                                            *");
+        
+        System.out.println("\n\n*****************************************");
+                          
+    }
+    }
+
