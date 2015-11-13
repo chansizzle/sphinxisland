98320555ef61fa941c6f70eda940c27699c738cf
chandlel <chandlel@CHANDLEL4.auth.hpicorp.net>
2015-11-13 15:26:30 -0700

    11/13/15 cleanded the code up.

--- sphinxIsland/src/byui/cit260/sphinxIsland/view/GameMenuView.java
+++ sphinxIsland/src/byui/cit260/sphinxIsland/view/GameMenuView.java
@@ -16,25 +16,24 @@ public class GameMenuView {
     public String getGAMEMENU() {
         return MENU;
     }
-    
+
     private final String MENU = "\n"
-            +"\n**********************************************************"
-            +"\n* Game Menu                                              *"
-            +"\n*\t B - Bag - Inventory within bag (map, gems, etc) *"
-            +"\n*\t M - Move to a different location                *"
-            +"\n*\t S - View list of Sphinxes encountered so far    *"
-            +"\n*\t P - View list of puzzles solved so far          *"
-            +"\n*\t L - Leave the island by boarding the Raft!      *"
-            +"\n*\t H - Help!                                       *"
-            +"\n*\t Q - Quit and return to the Main Menu            *"
-            +"\n**********************************************************";
-    
+            + "\n**********************************************************"
+            + "\n* Game Menu                                              *"
+            + "\n*\t B - Bag - Inventory within bag (map, gems, etc) *"
+            + "\n*\t M - Move to a different location                *"
+            + "\n*\t S - View list of Sphinxes encountered so far    *"
+            + "\n*\t P - View list of puzzles solved so far          *"
+            + "\n*\t L - Leave the island by boarding the Raft!      *"
+            + "\n*\t H - Help!                                       *"
+            + "\n*\t Q - Quit and return to the Main Menu            *"
+            + "\n**********************************************************";
 
     public boolean doAction(Object obj) {
         String value = (String) obj;
         value = value.toUpperCase();
-        
-       switch (value) {
+
+        switch (value) {
             case "B": // enter bag (inventory) to view map, retrieve gems, etc
             case "b":
                 this.bagInventory();
@@ -54,53 +53,51 @@ public class GameMenuView {
             case "L":
             case "l":
                 this.boardTheRaft();
-                break;               
+                break;
             case "H":
             case "h":
                 this.display();
                 break;
             case "Q": // return to main menu
-                case "q":
+            case "q":
                 this.display();
                 break;
-            default: 
+            default:
                 System.out.println("\n*** Invalid selection, please try again ***");
-             //   break;
-       }
-       return false;
+            //   break;
+        }
+        return false;
     }
 
-
-    
     private void moveLocations() {
         System.out.println("\n*** moveLocations function called ***");
     }
-    
+
     private void checkSphinxesVisited() {
         SphinxesVisitedView sphinxesVisited = new SphinxesVisitedView();
         sphinxesVisited.displaySphinxesVisited();
     }
-    
+
     private void puzzlesSolved() {
         System.out.println("\n*** puzzlesSolved function called ***");
     }
-    
-        private void boardTheRaft() {
+
+    private void boardTheRaft() {
         BoardRaftView raftView = new BoardRaftView();
         raftView.displayRaft();
     }
-    
+
     public void displayMenu() {
         MainMenuView mainMenu = new MainMenuView();
         mainMenu.display();
-    }     
-        
-   public void display() {
+    }
+
+    public void display() {
         HelpMenuView helpMenu = new HelpMenuView(); //need to device way for help menu in game menu to return to game menu
         helpMenu.display();
     }
- 
-   public void bagInventory() {
+
+    public void bagInventory() {
         // display the bag's inventory menu
         InventoryMenuView inventoryMenu = new InventoryMenuView();
         inventoryMenu.display();
