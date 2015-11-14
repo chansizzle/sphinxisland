0b9f6b7c3c889b57f84f5cf99a7fae7c2642d183
sarahbroat <sarahbroat@192.168.1.8>
2015-11-14 17:55:27 -0500

    11/14/2015 - Committing changes to merge

--- sphinxIsland/src/byui/cit260/sphinxIsland/view/MainMenuView.java
+++ sphinxIsland/src/byui/cit260/sphinxIsland/view/MainMenuView.java
@@ -6,7 +6,6 @@
 package byui.cit260.sphinxIsland.view;
 
 import byui.cit260.sphinxIsland.control.GameControl;
-import java.util.Scanner;
 import sphinxisland.SphinxIsland;
 
 /**
@@ -16,77 +15,72 @@ import sphinxisland.SphinxIsland;
 public class MainMenuView extends View {
 
     public MainMenuView() {
-    
-    
-    
-    super("\n"
-            +"\n------------------------------------"
-            +"\n| Main Menu                        |"
-            +"\n|\t N - Start new game             |"
-            +"\n|\t G - Start saved game           |"
-            +"\n|\t H - Get help with the game     |"
-            +"\n|\t S - Save the game              |"
-            +"\n|\t Q - Quit                       |"
-            +"\n------------------------------------");
+        super("\n"
+                + "\n------------------------------------"
+                + "\n| Main Menu                        |"
+                + "\n|\t N - Start new game             |"
+                + "\n|\t G - Start saved game           |"
+                + "\n|\t H - Get help with the game     |"
+                + "\n|\t S - Save the game              |"
+                + "\n|\t Q - Quit                       |"
+                + "\n------------------------------------");
     }
 
-    
+    @Override
     public boolean doAction(Object obj) {
-        String value = (String) obj;
-        value = value.toUpperCase();
-        
-       switch (value) {
-            case "N": // start a new game
-            case "n":
+        char selection = (char) obj;
+        //selection = selection.toUpperCase();
+
+        switch (selection) {
+            case 'N': // start a new game
+            case 'n':
                 this.startNewGame();
                 break;
-            case "G": // start a saved game
-            case "g":
+            case 'G': // start a saved game
+            case 'g':
                 this.startSavedGame();
                 break;
-            case "H": // access the help menu
-            case "h":
-                this.display();
+            case 'H': // access the help menu
+            case 'h':
+                this.displayHelpMenu();
                 break;
-            case "S": //save the current game
-            case "s":
+            case 'S': //save the current game
+            case 's':
                 this.saveGame();
                 break;
-            case "E": // exit the program
-            case "e":
+            case 'Q': // exit the program
+            case 'q':
                 System.exit(0);
                 //Runtime.getRuntime().halt(0);
                 break;
             default:
-            System.out.println("\n*** Invalid selection, please try again ***");
-                //break;
-       }
-       return false;
+                System.out.println("\n*** Invalid selection, please try again ***");
+            //break;
+        }
+        return false;
     }
 
     private void startNewGame() {
         // create a new a game 
         GameControl.createNewGame(SphinxIsland.getPlayer());
-        
-                       //goes to void displayGameMenu() in gamemenuview to start MENU in Gameplay
+
+        //goes to void displayGameMenu() in gamemenuview to start MENU in Gameplay
     }
 
     private void startSavedGame() {
         System.out.println("*** startSavedGame function called ***");
     }
 
-
-
     private void saveGame() {
         System.out.println("*** saveGame function called ***");
     }
-    
+
     public void displayMenu() {
         MainMenuView mainMenu = new MainMenuView();
         mainMenu.display();
-    } 
-    
-    public void display() {
+    }
+
+    public void displayHelpMenu() {
         HelpMenuView helpMenu = new HelpMenuView(); //need to device way for help menu in game menu to return to game menu
         helpMenu.display();
     }
